983a3d01367f710d44c2fde8ef3bd08f776fda0c
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-12-20 13:21:37 -0500

    feat: :rocket: i haven't idea

--- src/shared/components/Footer.tsx
+++ src/shared/components/Footer.tsx
@@ -9,7 +9,7 @@ export const Footer = () => {
           <span className="Alexi">Alexi Dg</span>.
         </p>
         <div className="flex justify-center space-x-4 mt-4">
-          <a href="https://www.facebook.com/alexis.duran.54772728" className="text-gray-400 hover:text-white">
+          <a href="https://www.facebook.com/alexis.duran.54772728" className="text-gray-400 hover:text-blue-600">
             <Facebook />
             {/* <i className="fab fa-facebook"></i> */}
           </a>
@@ -17,11 +17,11 @@ export const Footer = () => {
             <Github />
             {/* <i className="fab fa-github"></i> */}
           </a>
-          <a href="https://www.instagram.com/alexis_duran_dg/" className="text-gray-400 hover:text-white">
+          <a href="https://www.instagram.com/alexis_duran_dg/" className="text-gray-400 hover:text-pink-600">
             <Instagram />
             {/* <i className="fab fa-instagram"></i> */}
           </a>
-          <a href="https://www.linkedin.com/in/alexi-duran-gomez-6b17042a3/" className="text-gray-400 hover:text-white">
+          <a href="https://www.linkedin.com/in/alexi-duran-gomez-6b17042a3/" className="text-gray-400 hover:text-blue-600">
             <Linkedin />
             {/* <i className="fab fa-linkedin"></i> */}
           </a>
