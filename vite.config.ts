6944c312636d06fd401d81d0f80fa892bdfa87b3
Alexi Dg <alexisdurangomez588@gmail.com>
2026-04-14 21:43:46 -0500

    test: :white_check_mark: add vitest frontend setup

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    globals: true,
+  },
 });
