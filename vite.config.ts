a5832e2e72ae102382bd920920950c4012d0074d
Alexi Dg <alexisdurangomez588@gmail.com>
2026-04-14 22:00:29 -0500

    fix: :bug: split vite and vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -12,9 +12,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  test: {
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.ts",
-    globals: true,
-  },
 });
